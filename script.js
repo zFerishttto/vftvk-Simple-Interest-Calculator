1a91eacb518b0be8192d182e7f716992a2f845c9
zFerishttto <108380714+zFerishttto@users.noreply.github.com>
2022-06-30 00:24:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p,t,r,si,ci;
+    p = document.getElementById ("first").value;
+    t = document.getElementById ("second").value;
+    r = document.getElementById ("third").value;
+    si = parseInt((p*t*r)/100 );
+    amount = p*Math.pow((1 +r/100),t );
+    ci = amount-p;
+    document.getElementById ('num').innerHTML ="Simple interest : "+si;
+    document.getElementById ('num1').innerHTML ="Compound interest : "+ci;
+
 }
         
